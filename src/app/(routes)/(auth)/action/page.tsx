843ec392f6751919cc99b044111767c31ab56bb1
ShaharearSabbir <imshaharear@gmail.com>
2025-09-15 13:31:46 +0600

    folder stucture

---
+++ src/app/(routes)/(auth)/action/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const page = () => {
+    // will grab the url and based on the mode action will be decided and will verify user using jtw which will be sent via email
+    return (
+        <div>
+            {/* action for reset password, forget email, verify email */}
+        </div>
+    );
+};
+
+export default page;
