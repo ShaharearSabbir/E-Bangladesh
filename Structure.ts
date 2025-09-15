94a75e5ea76df62772ad5678f6fa7280d6152c54
Sazid Ahamed Sifat <147806245+SazidSifat@users.noreply.github.com>
2025-09-16 01:36:20 +0600

    Revert "Added registration and login page"

--- Structure.ts
+++
@@ -1,4 +0,0 @@
-const BCForm = {
-    name: "name",
-    DOB: "dfvsdfsd"
-}
--- src/app/(routes)/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const page = () => {
-  // Dashboard will use component based on their section
-  return <div>{/* Admin Dashboard */}</div>;
-};
-
-export default page;
