083ca2091cda030ea1a9632f57ad0f1d1eb6e8ad
ShaharearSabbir <imshaharear@gmail.com>
2025-09-18 18:26:39 +0600

    SMIP setup

--- src/lib/mailer/mailer.ts
+++ src/lib/mailer/mailer.ts
@@ -1,22 +1,24 @@
+"use server"
+
 import nodemailer from "nodemailer";
 
 interface MailerArgs {
-    to: string,
-    subject: string
-    body: 
+  to: string;
+  subject: string;
+  body: string;
 }
 
-export const sendMail = async ({ to, subject, body }: MailerArgs) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "electrotic.bangladesh@gmail.com",
-      pass: process.env.GMAIL_APP_PASSWORD,
-    },
-  });
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "electrotic.bangladesh@gmail.com",
+    pass: process.env.GMAIL_APP_PASSWORD as string,
+  },
+});
 
+export const sendMail = async ({ to, subject, body }: MailerArgs) => {
   const mailOptions = {
-    from: process.env.EMAIL_USER,
+    from: "electrotic.bangladesh@gmail.com",
     to: to,
     subject: subject,
     html: body,
